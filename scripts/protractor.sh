59d6de315f056a7179c7e1704e038efa028da362
Sebastian Tschan <mail@blueimp.net>
2015-06-18 15:53:56 +0200

    Use public protractor docker image.

--- scripts/protractor.sh
+++ scripts/protractor.sh
@@ -17,12 +17,18 @@
 # This allows to select debug nodes with integrated VNC server:
 # Example: SELENIUM_NODES="chrome-debug" ./protractor.sh
 #
+# The SELENIUM_HUB environment variable can be set to a running hub container.
+# Example: SELENIUM_HUB=$(docker run -d $SELENIUM_IMAGE) ./protractor.sh
+#
 # Copyright 2015, Sebastian Tschan
 # https://blueimp.net
 #
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/MIT
 #
+
+# Exit immediately if a command exits with a non-zero status:
+set -e
 
 if [ -z "$PROTRACTOR_VERSION" ]
 then
@@ -31,7 +37,7 @@ fi
 
 if [ -z "$SELENIUM_VERSION" ]
 then
-  SELENIUM_VERSION="2.45.0"
+  SELENIUM_VERSION="2.46.0"
 fi
 
 if [ -z "$SELENIUM_NODES" ]
@@ -45,9 +51,7 @@ NODES[1]="selenium/node-firefox:$SELENIUM_VERSION"
 NODES[2]="selenium/node-chrome-debug:$SELENIUM_VERSION"
 NODES[3]="selenium/node-firefox-debug:$SELENIUM_VERSION"
 
-LABEL="protractor=$PROTRACTOR_VERSION"
-IMAGE="protractor:$PROTRACTOR_VERSION"
-SELENIUM_LABEL="selenium-hub=$SELENIUM_VERSION"
+PROTRACTOR_IMAGE="blueimp/protractor:$PROTRACTOR_VERSION"
 SELENIUM_IMAGE="selenium/hub:$SELENIUM_VERSION"
 
 WORKDIR="/home/protractor"
@@ -56,61 +60,36 @@ VOLUME="$PWD:$WORKDIR"
 HOSTNAME="hub"
 URL="http://$HOSTNAME:4444/wd/hub"
 
-DOCKERFILE="
-FROM node:0.10
-
-# node-gyp dependency build requires --unsafe-perm option:
-RUN npm install -g --unsafe-perm protractor@$PROTRACTOR_VERSION
-
-# Add protractor system group/user with gid/uid 1000.
-# This is a workaround for boot2docker issue #581, see
-# https://github.com/boot2docker/boot2docker/issues/581
-RUN groupadd -g 1000 protractor
-RUN useradd -g protractor -u 1000 protractor
-
-USER protractor
-WORKDIR $WORKDIR
-ENTRYPOINT [\"protractor\"]
-
-LABEL $LABEL
-"
-
-# Check if protractor image exists:
-if [ -z "$(docker images -q -f label=$LABEL)" ]
-then
-  # Build and tag protractor image:
-  echo "$DOCKERFILE" | docker build -t protractor -
-  docker tag protractor:latest $IMAGE
-fi
-
-# Check if selenium hub has been created:
-HUB=$(docker ps -a -q -f label=$SELENIUM_LABEL)
-
-if [ -z "$HUB" ]
-then
-  # Start selenium hub:
-  HUB=$(docker run -d -l $SELENIUM_LABEL $SELENIUM_IMAGE)
-else
-  HUB=$(docker restart $HUB)
-fi
-
-CIDS=()
-
 function cleanup {
   # Stop and remove browser nodes:
   CIDS=$(docker stop ${CIDS[@]})
   CIDS=$(docker rm -v $CIDS)
-  # Stop the selenium hub:
-  HUB=$(docker stop $HUB)
+  if [ -z "$SELENIUM_HUB" ]
+  then
+    # Stop and remove the selenium hub:
+    HUB=$(docker stop $HUB)
+    HUB=$(docker rm -v $HUB)
+  fi
 }
 
 # Clean up on SIGTERM and EXIT:
 trap "cleanup; exit" SIGTERM EXIT
 
+if [ -z "$SELENIUM_HUB" ]
+then
+  # Start selenium hub:
+  HUB=$(docker run -d $SELENIUM_IMAGE)
+else
+  # Use existing hub:
+  HUB=$SELENIUM_HUB
+fi
+
 LINK=$HUB:$HOSTNAME
 
 # Only run the matching nodes:
 NODES=($(printf "%s\n" "${NODES[@]}" | grep -E "$SELENIUM_NODES"))
+
+CIDS=()
 
 # Start and register browser nodes:
 for INDEX in ${!NODES[@]}
@@ -133,4 +112,4 @@ done
 set -- "$@" --seleniumAddress "$URL"
 
 # Run the given e2e test suite:
-docker run --rm -v "$VOLUME" --link $LINK $IMAGE "$@"
+docker run --rm -v "$VOLUME" --link $LINK $PROTRACTOR_IMAGE "$@"
