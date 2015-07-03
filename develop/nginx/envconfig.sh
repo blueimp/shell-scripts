1daf1d0a666398345a9362f8f52a2de6cbfb26e1
Sebastian Tschan <mail@blueimp.net>
2015-07-03 11:33:08 +0200

    Add -e option to pass envconfig configuration as environment variable. Make envconfig script POSIX compatible.

--- develop/nginx/envconfig.sh
+++ develop/nginx/envconfig.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # Wrapper script to write environment variables in config files.
 # Replaces placeholders and creates files, then starts the given command.
 #
-# Usage: ./envconfig.sh [-f config] [command] [args...]
+# Usage: ./envconfig.sh [-e config_env] [-f config_file] [command] [args...]
 #
-# The default configuration file is "/usr/local/etc/envconfig.conf".
-# An alternate config file can be provided via -f option.
+# The default envconfig configuration file is "/usr/local/etc/envconfig.conf".
+# An alternate configuration file can be provided via -f option.
+# An environment variable with configuration can be provided via -e option.
+# Mappings provided via environment variable will be written first.
 #
-# Each line of the config file must have the following format:
-# VARIABLE_NAME file_path
+# Each line of the configuration for envconfig must have the following format:
+# VARIABLE_NAME /absolute/path/to/config/file
+# Empty lines and lines starting with a hash (#) will be ignored.
+# Multiple mappings of the same VARIABLE_NAME or path are possible.
 #
-# Placeholders in configuration files must have the following format:
+# Placeholders in config files must have the following format:
 # {{VARIABLE_NAME}}
 #
 # Copyright 2015, Sebastian Tschan
@@ -34,19 +38,19 @@ find_and_replace() {
 write_config() {
   while read line
   do
-    # Skip empty lines:
-    if [[ -z "$line" ]]
+    # Skip empty lines and lines starting with a hash (#):
+    if [ -z "$line" ] || [ "${line#\#}" != "$line" ]
     then
       continue
     fi
     # Extract the substring up to the first space as variable name:
-    local name=${line%% *}
+    local name="${line%% *}"
     # Extract the remainder as path and trim any surrounding whitespace:
-    local path=$(echo ${line#* })
+    local path="$(echo ${line#* })"
     # Evaluate the name as environment variable, print error if unset:
-    local value=$(eval 'echo "${'$name'?}"')
+    local value="$(eval 'echo "${'$name'?}"')"
     # Check if the file exists and has a size greater than zero:
-    if [[ -s "$path" ]]
+    if [ -s "$path" ]
     then
       # Replace the placeholder with the environment variable:
       find_and_replace "{{$name}}" "$value" "$path"
@@ -60,12 +64,23 @@ write_config() {
   done < "$1"
 }
 
+# Check if envconfig configuration is provided via environment variable:
+if [ "$1" = "-e" ]
+then
+  # Write the environment variable to a temporary file, fail if unset:
+  eval 'echo "${'$2'?}"' > /tmp/envconfig.conf
+  # Use the temporary file to write the env config:
+  write_config /tmp/envconfig.conf
+  # Shift the arguments list to remove the given -e option:
+  shift 2
+fi
+
 # Check if the config file is provided via command line:
 if [ "$1" = "-f" ]
 then
   # Use the given config file to write the env config:
   write_config "$2"
-  # Remove the config file parameters from the arguments list:
+  # Shift the arguments list to remove the given -f option:
   shift 2
 else
   # Use the default config file to write the env config:
