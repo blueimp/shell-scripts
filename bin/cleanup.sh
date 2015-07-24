39597097f8c9605c0ffe374d2ab767f12d9a98ef
Sebastian Tschan <mail@blueimp.net>
2015-07-24 13:03:06 +0200

    Add script to remove dangling docker images.

---
+++ bin/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#
+# Removes dangling docker images.
+#
+# Usage: ./cleanup.sh
+#
+# Copyright 2015, Sebastian Tschan
+# https://blueimp.net
+#
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT
+#
+
+# Remove dangling docker images:
+docker rmi $(docker images -f 'dangling=true' -q)
