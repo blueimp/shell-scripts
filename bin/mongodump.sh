c21468bdc7092e37236d6628812444046b705195
Sebastian Tschan <mail@blueimp.net>
2015-08-14 23:18:35 +0200

    Refactored mongodump and mongorestore to be POSIX shell compatible.

--- bin/mongodump.sh
+++ bin/mongodump.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # Run mongodump in a docker container.
@@ -14,58 +14,74 @@
 # Exit immediately if a command exits with a non-zero status:
 set -e
 
-MONGODB_USER=${MONGODB_USER:-mongodb}
+MONGODB_USER="${MONGODB_USER:-mongodb}"
 
-if [[ -z "$MONGODB_CONTAINER" ]]; then
+if [ -z "$MONGODB_CONTAINER" ]; then
 	echo "Usage: MONGODB_CONTAINER=CONTAINER_ID $0" >&2
 	exit 1
 fi
 
 if [ "$1" = "--help" ] || [ "$1" = "--version" ]; then
-	docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongodump $@ || exit 1
-	exit
+	docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongodump $@
+	exit $?
 fi
 
-TMPDIR=$(mktemp -d /tmp/mongodb-dump-XXXXXXXXXX)
+TMPDIR="$(mktemp -d /tmp/mongodb-dump-XXXXXXXXXX)"
 
-function cleanup() {
+cleanup() {
 	rm -rf "$TMPDIR"
 }
 
 # Clean up on exit:
-trap "EXITCODE=$?; cleanup; exit $EXITCODE" SIGTERM EXIT
+trap "EXITCODE=$?; cleanup; exit $EXITCODE" TERM EXIT
 
 HOSTDIR=""
+RESET=""
+LAST_ARG=""
 
-for ((i=1; i<=$#; i++)); do
-	# Check if dump target is set:
-	if [ "${!i}" = "--out" ]; then
-		HOSTDIR=${@:i+1:1}
-		# Replace host dir argument with temp dir:
-		set -- "${@:1:i}" "$TMPDIR" "${@:i+2:$#}"
-		break
+# Loop over the arguments to adjust the dump target:
+for arg; do
+	if [ -z "$RESET" ]; then
+		# Reset the arguments list at the start of the loop:
+		set --
+		RESET="true"
 	fi
+	# Check for a dump target in "--out=dump/" format:
+	if [ ! -z "${arg#--out=}" ]; then
+		arg="${arg#--out=}"
+		set -- "$@" -o
+		LAST_ARG="-o"
+	fi
+	# Check for a dump target in "-o dump/" or  "--out dump/" format:
+	if [ "$LAST_ARG" = "-o" ] || [ "$LAST_ARG" = "--out" ]; then
+		HOSTDIR="$arg"
+		# Set the temp dir as new dump target:
+		set -- "$@" "$TMPDIR"
+		LAST_ARG=""
+		continue
+	fi
+	# Rebuild the arguments list with each iteration:
+	set -- "$@" "$arg"
+	LAST_ARG="$arg"
 done
 
-if [[ -z "$HOSTDIR" ]]; then
+if [ -z "$HOSTDIR" ]; then
 	# Use default dump target as host dir:
 	HOSTDIR="$PWD/dump"
 	# Set dump target to temp dir:
-	set -- "$@" --out "$TMPDIR"
+	set -- "$@" -o "$TMPDIR"
 fi
 
 mkdir -p "$HOSTDIR"
 cd "$HOSTDIR"
 
 # Export dump data to host dir:
-docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongodump $@
-docker exec -u $MONGODB_USER $MONGODB_CONTAINER test -d $TMPDIR || exit 0
+docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongodump "$@"
+docker exec $MONGODB_CONTAINER test -d $TMPDIR || exit 0
 docker cp $MONGODB_CONTAINER:"$TMPDIR" .
-docker exec -u $MONGODB_USER $MONGODB_CONTAINER rm -rf "$TMPDIR"
+docker exec $MONGODB_CONTAINER rm -rf "$TMPDIR"
 
-# Move the dump data out of the temp dir:
-BASENAME=$(basename $TMPDIR)
-mv $BASENAME/* .
-rmdir $BASENAME
-
-exit 0
+# Move the dump data out of the temp dir basename:
+BASENAME="$(basename $TMPDIR)"
+cp -r $BASENAME/* .
+rm -rf $BASENAME
--- bin/mongorestore.sh
+++ bin/mongorestore.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # Run mongorestore in a docker container.
@@ -14,49 +14,60 @@
 # Exit immediately if a command exits with a non-zero status:
 set -e
 
-MONGODB_USER=${MONGODB_USER:-mongodb}
+MONGODB_USER="${MONGODB_USER:-mongodb}"
 
-if [[ -z "$MONGODB_CONTAINER" ]]; then
+if [ -z "$MONGODB_CONTAINER" ]; then
 	echo "Usage: MONGODB_CONTAINER=CONTAINER_ID $0" >&2
 	exit 1
 fi
 
 if [ "$1" = "--help" ] || [ "$1" = "--version" ]; then
-	docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongorestore $@ || exit 1
-	exit
+	docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongorestore $@
+	exit $?
 fi
 
-TMPDIR=$(mktemp -d /tmp/mongodb-dump-XXXXXXXXXX)
+TMPDIR="$(mktemp -d /tmp/mongodb-dump-XXXXXXXXXX)"
 
-function cleanup() {
+cleanup() {
 	rm -rf "$TMPDIR"
 }
 
 # Clean up on exit:
-trap "EXITCODE=$?; cleanup; exit $EXITCODE" SIGTERM EXIT
+trap "EXITCODE=$?; cleanup; exit $EXITCODE" TERM EXIT
 
-HOSTDIR="${@: -1}"
+HOSTDIR=""
+RESET=""
+COUNT=$#
+INDEX=0
 
-if [ -d "$HOSTDIR" ]; then
-	# Keep all but the last argument:
-	ARGS="${@:1:$#-1}"
-else
-	# Use default dump target as host dir:
+# Loop over the arguments list to rebuild it:
+for arg; do
+	if [ -z "$RESET" ]; then
+		# Reset the arguments list at the start of the loop:
+		set --
+		RESET="true"
+	fi
+	INDEX=$(($INDEX+1))
+	if [ $INDEX = $COUNT ] && [ -d "$arg" ]; then
+		HOSTDIR="$arg"
+		# Replace the dump source with the temp dir:
+		arg="$TMPDIR"
+	fi
+	# Rebuild the arguments list with each iteration:
+	set -- "$@" "$arg"
+done
+
+if [ -z "$HOSTDIR" ]; then
+	# Use the default dump target as host dir:
 	HOSTDIR="$PWD/dump"
-	# Keep all arguments:
-	ARGS="$@"
+	# Set the temp dir as dump source:
+	set -- "$@" "$TMPDIR"
 fi
 
 cd "$HOSTDIR"
 
-docker exec -u $MONGODB_USER $MONGODB_CONTAINER mkdir -p "$TMPDIR"
-
-# Set host dir argument to temp dir:
-set -- "$ARGS" "$TMPDIR"
-
-# Import dump data into the running mongo container:
+# Import the dump data into the running mongo container:
+docker exec $MONGODB_CONTAINER mkdir -p "$TMPDIR"
 docker cp . $MONGODB_CONTAINER:"$TMPDIR"
 docker exec -u $MONGODB_USER $MONGODB_CONTAINER mongorestore $@
 docker exec $MONGODB_CONTAINER rm -rf "$TMPDIR"
-
-exit 0
