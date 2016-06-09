deade574090620882add90291b4e0cc75eeb68b5
Sebastian Tschan <mail@blueimp.net>
2016-06-09 02:32:15 +0200

    Add description and usage comment.

--- bin/docker-image-cleanup.sh
+++ bin/docker-image-cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # shellcheck shell=dash
 
+#
+# Removes dangling docker images.
+#
+# Usage: ./docker-image-cleanup.sh
+#
+
 # Remove dangling docker images:
 # shellcheck disable=SC2046
 docker rmi $(docker images -f 'dangling=true' -q)
