0bbf031b51cf0ca130aeb1a425fa8c0209f9b172
Sebastian Tschan <mail@blueimp.net>
2018-02-20 13:49:02 +0100

    Allow querying for other tags.

--- bin/aws-ec2-private-ip.sh
+++ bin/aws-ec2-private-ip.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 #
-# Returns the private IP of the EC2 instance with the given "Name" tag.
+# Returns the private IP of the EC2 instance with the given tag.
+# By default the Name tag is queried.
+# A different tag key can be provided via --tag option.
 #
-# For multiple instances with the same name, an index after the name indicates
+# For multiple instances with the same tag, an index after the value indicates
 # for which instance the private IP address should be returned.
 # e.g. "name" and "name0" return the first, "name1" the second, etc.
 #
-# Usage: ./aws-ec2-private-ip.sh name[index]
+# Usage: ./aws-ec2-private-ip.sh [--tag key] value[index]
 #
 # Example `~/.ssh/config` for an SSH Bastion setup:
 #
@@ -27,10 +29,18 @@
 
 set -e
 
-NAME=${1:?}
+if [ "$1" = --tag ]; then
+  TAG=$2
+  shift 2
+else
+  TAG=Name
+fi
+
+VALUE=$(echo "$1" | sed 's/[0-9]$//g')
+INDEX=$((${1##$VALUE}+1))
 
 aws ec2 describe-instances \
   --no-paginate \
-  --filters "Name=tag:Name,Values=$(echo "$NAME" | sed 's/[0-9]//g')" \
+  --filters "Name=tag:$TAG,Values=$VALUE" \
   --output text --query 'Reservations[*].Instances[*].PrivateIpAddress' |
-  awk "NR==$(($(echo "$NAME" | sed 's/[^0-9]//g')+1))"
+  awk "NR==$INDEX"
