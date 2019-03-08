e86d40b948b4133011f0bc5fb6a77819f00e791a
Sebastian Tschan <mail@blueimp.net>
2019-03-08 16:18:28 +0100

    Accept a list of files and directories.

--- bin/apply-commit-times.sh
+++ bin/apply-commit-times.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #
-# Sets the modification time of all files in the given directory to their
-# respective git commit timestamps.
+# Sets the modification times of the given files and the files in the given
+# directories to their respective git commit timestamps.
 #
-# Usage: ./apply-commit-times.sh [directory]
+# Usage: ./apply-commit-times.sh directory|file [...]
 #
 # Copyright 2017, Sebastian Tschan
 # https://blueimp.net
@@ -19,17 +19,25 @@ set -e
 export TZ=UTC0
 
 apply_commit_time() {
-  # Check if this is a readable file:
-  if [ ! -r "$1" ]; then return; fi
   # Extract the commit date for the file in `touch -t` format (CCYYMMDDhhmm.ss):
   timestamp=$(git log -1 --format=%cd --date=format-local:%Y%m%d%H%M.%S -- "$1")
-  # Set the modification time of the given file to the commit timestamp:
-  touch -t "$timestamp" "$1"
+  if [ -n "$timestamp" ]; then
+    # Set the modification time of the given file to the commit timestamp:
+    touch -t "$timestamp" "$1"
+  fi
 }
 
-# The "-z" option of `git ls-tree` outputs NUL byte separated file paths, which
-# can be iterated over with the "-d" option of the `read` built-in, which is
-# not available in POSIX shell. However, this allows us to handle any file name
-# which is valid on UNIX systems:
-git ls-tree -r -z --name-only HEAD -- "${1:-.}" | while IFS= read -d '' -r FILE
-do apply_commit_time "$FILE"; done
+while [ $# -gt 0 ]; do
+  # Is the argument a directory path?
+  if [ -d "$1" ]; then
+    # The "-z" option of `git ls-tree` outputs NUL byte separated file paths,
+    # which can be iterated over with the "-d" option of the `read` built-in.
+    # Since this option not available in POSIX shell, bash is required:
+    git ls-tree -r -z --name-only HEAD -- "$1" | while IFS= read -d '' -r FILE
+    do apply_commit_time "$FILE"; done
+  # Else is the argument a path to a readable file?
+  elif [ -r "$1" ]; then
+    apply_commit_time "$1"
+  fi
+  shift
+done
