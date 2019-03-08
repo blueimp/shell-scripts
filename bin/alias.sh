e039c69272ca261ac9af33d2d38c3b79ea5eef9a
Sebastian Tschan <mail@blueimp.net>
2019-03-08 11:09:34 +0100

    Move the random script into an alias.

--- bin/alias.sh
+++ bin/alias.sh
@@ -11,3 +11,6 @@ alias srv='python3 -m http.server --bind 127.0.0.1'
 
 # Print listening services:
 alias listening='lsof -iTCP -sTCP:LISTEN -n -P +c0'
+
+# Prints a random alphanumeric characters string with the given length:
+alias random='LC_ALL=C tr -dc A-Za-z0-9 < /dev/urandom | head -c'
